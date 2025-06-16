899bde90691a4c6ba20ef71761dfb684713a6b5d
unknown <bs7720405@gmail.com>
2025-06-16 22:03:45 +0300

    create .env file

--- index.js
+++ index.js
@@ -2,27 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config();
 const mongoose = require('mongoose');
-require('dotenv').config(); // אם את מריצה מקומית
 
 const ordersRoutes = require('./routes/orders_routes');
 
 app.use(express.json());
-
 app.use('/api/orders', ordersRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('✅ Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
     .catch((err) => {
         console.error('❌ MongoDB connection error:', err);
     });
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
